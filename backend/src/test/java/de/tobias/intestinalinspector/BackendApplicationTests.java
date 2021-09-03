a5a2c05ff7742208fba9ab447af031ca75796012
Tobias Hahn <post@tobias-hahn.eu>
2021-09-03 11:28:20 +0200

    init backend

---
+++ backend/src/test/java/de/tobias/intestinalinspector/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tobias.intestinalinspector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
