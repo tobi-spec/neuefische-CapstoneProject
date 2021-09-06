dd443fc9ce2fee6ad84c385242d3d3035352d3df
Tobias Hahn <post@tobias-hahn.eu>
2021-09-06 10:41:36 +0200

    add FoodEntity for Database

---
+++ backend/src/main/java/de/tobias/intestinalinspector/model/FoodEntity.java
@@ -0,0 +1,49 @@
+package de.tobias.intestinalinspector.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "food_table")
+@Getter
+@Setter
+public class FoodEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private long id;
+
+
+    @Column(name= "food_name")
+    private String foodName;
+
+    @Column(name="date")
+    private Date date;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FoodEntity that = (FoodEntity) o;
+        return id == that.id && Objects.equals(foodName, that.foodName) && Objects.equals(date, that.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, foodName, date);
+    }
+
+    @Override
+    public String toString() {
+        return "FoodEntity{" +
+                "id=" + id +
+                ", foodName='" + foodName + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
