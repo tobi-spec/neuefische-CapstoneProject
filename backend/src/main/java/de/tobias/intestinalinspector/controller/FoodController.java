c78bd4612c1d64ffd07a64d96a32002c386f0524
Tobias Hahn <post@tobias-hahn.eu>
2021-09-06 12:06:45 +0200

    bugfix in foodcontroller add method

--- backend/src/main/java/de/tobias/intestinalinspector/controller/FoodController.java
+++ backend/src/main/java/de/tobias/intestinalinspector/controller/FoodController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/food)")
+@RequestMapping("api/food")
 public class FoodController {
 
     FoodService foodService;
