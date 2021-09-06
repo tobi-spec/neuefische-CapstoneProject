ffb5a91affb9d37f32dd1689977e7ba0ea817c16
Tobias Hahn <post@tobias-hahn.eu>
2021-09-06 15:43:05 +0200

    build up addFood route to backend/database

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,12 +1,29 @@
 import styled from 'styled-components'
+import axios from 'axios'
+import { useState } from 'react'
 
 export default function Main() {
+  const [food, setFood] = useState('')
+
+  const foodHandler = event =>
+    setFood({
+      foodName: event.target.value,
+    })
+
+  function addFood() {
+    axios
+      .post('/api/food', food)
+      .then(response => console.log(food))
+      .catch(error => console.error(error))
+  }
+
   return (
     <section>
       <Wrapper>
-        <h1>How do you feel?</h1>
+        <h1>How do you feel? :)</h1>
         <p>I have eaten:</p>
-        <input type="text" />
+        <input type="text" onChange={foodHandler} />
+        <button onClick={addFood}>send</button>
       </Wrapper>
     </section>
   )
@@ -15,10 +32,11 @@ export default function Main() {
 const Wrapper = styled.div`
   display: grid;
   height: 100vh;
-
-  grid-template-rows: 4fr 1fr 1fr 4fr;
+  grid-template-rows: 4fr 1fr 1fr 1fr 4fr;
 
   justify-content: center;
   justify-items: center;
   align-items: center;
+
+  background-color: lightgreen;
 `
