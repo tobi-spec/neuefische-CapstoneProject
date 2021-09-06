ef6abd34046101856e475fe0acf089c2e32c2608
Tobias Hahn <post@tobias-hahn.eu>
2021-09-06 08:55:42 +0200

    add empty FoodController and empty FoodControllerTest

---
+++ backend/src/main/java/de/tobias/intestinalinspector/controller/FoodController.java
@@ -0,0 +1,9 @@
+package de.tobias.intestinalinspector.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class FoodController {
+}
---
+++ backend/src/test/java/de/tobias/intestinalinspector/controller/FoodControllerTest.java
@@ -0,0 +1,11 @@
+package de.tobias.intestinalinspector.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest()
+class FoodControllerTest {
+
+}
