dcaf56edba2c9e352f0376836e167546ede2c949
Tobias Hahn <post@tobias-hahn.eu>
2021-09-07 14:59:32 +0200

    login controller

---
+++ backend/src/main/java/de/tobias/intestinalinspector/controller/LoginController.java
@@ -0,0 +1,61 @@
+package de.tobias.intestinalinspector.controller;
+
+
+import de.tobias.intestinalinspector.dto.AccessToken;
+import de.tobias.intestinalinspector.dto.AppUserDto;
+import de.tobias.intestinalinspector.dto.CredentialsDto;
+import de.tobias.intestinalinspector.model.AppUserEntity;
+import de.tobias.intestinalinspector.repository.AppUserRepository;
+import de.tobias.intestinalinspector.service.JwtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@CrossOrigin
+public class LoginController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final AppUserRepository appUserRepository;
+
+    public LoginController(AuthenticationManager authenticationManager, JwtService jwtService, AppUserRepository appUserRepository) {
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.appUserRepository = appUserRepository;
+    }
+
+    @GetMapping("auth/me")
+    public ResponseEntity<AppUserDto> getLoggedInUser(@AuthenticationPrincipal AppUserEntity appUser){
+        return ok(AppUserDto.builder()
+                .userName(appUser.getUserName())
+                .userRole(appUser.getUserRole())
+                .build());
+    }
+
+    @PostMapping("/auth/access_token")
+    public ResponseEntity<AccessToken> getAccessToken(@RequestBody CredentialsDto credentials) {
+        String username = credentials.getUserName();
+        String password = credentials.getUserPassword();
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        try{
+            authenticationManager.authenticate(authToken);
+
+            AppUserEntity appUser = appUserRepository.findByUserName(username).orElseThrow();
+            String token = jwtService.createJwtToken(appUser);
+
+            AccessToken accessToken = new AccessToken(token);
+            return ok(accessToken);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
