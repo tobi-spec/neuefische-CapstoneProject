29434cfa433f40b248437bc32f6046a86a900add
Tobias Hahn <post@tobias-hahn.eu>
2021-09-07 15:21:07 +0200

    fix typo

--- backend/src/main/java/de/tobias/intestinalinspector/config/JwtConfig.java
+++ backend/src/main/java/de/tobias/intestinalinspector/config/JwtConfig.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "securtiy.jwt")
+@ConfigurationProperties(prefix = "security.jwt")
 @Data
 public class JwtConfig {
 
