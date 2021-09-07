58d70c3c685767750aa7a263290206b8adfdb2c3
Tobias Hahn <post@tobias-hahn.eu>
2021-09-07 11:48:29 +0200

    add AppUser Entity

---
+++ backend/src/main/java/de/tobias/intestinalinspector/model/AppUserEntity.java
@@ -0,0 +1,50 @@
+package de.tobias.intestinalinspector.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUserEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name="id", nullable = false)
+    private long id;
+
+    @Column(name="user_name", nullable = false)
+    private String userName;
+
+    @Column(name="user_password", nullable = false)
+    private String userPassword;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUserEntity that = (AppUserEntity) o;
+        return id == that.id && Objects.equals(userName, that.userName) && Objects.equals(userPassword, that.userPassword);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, userPassword);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUserEntity{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                '}';
+    }
+}
