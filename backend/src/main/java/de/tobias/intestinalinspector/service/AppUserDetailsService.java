a32293e4615a6c4f1cec5ff813e7ce95e51c34d6
Tobias Hahn <post@tobias-hahn.eu>
2021-09-07 11:54:39 +0200

    Security Service

---
+++ backend/src/main/java/de/tobias/intestinalinspector/service/AppUserDetailsService.java
@@ -0,0 +1,33 @@
+package de.tobias.intestinalinspector.service;
+
+import de.tobias.intestinalinspector.model.AppUserEntity;
+import de.tobias.intestinalinspector.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    AppUserRepository appUserRepository;
+
+    @Autowired
+    public AppUserDetailsService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUserEntity appUser = appUserRepository
+                .findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("not found " + username));
+
+        return User.builder()
+                .username(appUser.getUserName())
+                .password(appUser.getUserPassword())
+                .build();
+    }
+}
