77a0e35bd17170e9244b29c94877dde0003318a8
Tobias Hahn <post@tobias-hahn.eu>
2021-09-08 11:05:11 +0200

    add Login to Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Main from './pages/Main'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/">
+          <Login />
+        </Route>
+        <Route path="/main">
           <Main />
         </Route>
       </Switch>
