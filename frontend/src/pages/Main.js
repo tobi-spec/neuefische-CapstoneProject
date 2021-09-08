62faae8a9829470192db7e0962075569c0bddd69
Tobias Hahn <post@tobias-hahn.eu>
2021-09-08 08:50:02 +0200

    changed from div to form

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -10,28 +10,28 @@ export default function Main() {
       foodName: event.target.value,
     })
 
-  function addFood() {
+  const submitHandler = event => {
+    event.preventDefault()
     axios
       .post('/api/food', food)
-      .then(response => console.log(food))
+      .then(response => console.log(response))
       .catch(error => console.error(error))
       .finally(() => setFood({ foodName: '' }))
   }
 
   return (
     <section>
-      <Wrapper>
+      <Wrapper onSubmit={submitHandler}>
         <h1>How do you feel? :)</h1>
         <p>I have eaten:</p>
         <input type="text" value={food.foodName} onChange={foodHandler} />
-        <button onClick={addFood}>send</button>
+        <button type="submit">send</button>
       </Wrapper>
     </section>
   )
 }
 
-const Wrapper = styled.div`
-  //form
+const Wrapper = styled.form`
   display: grid;
   height: 100vh;
   grid-template-rows: 4fr 1fr 1fr 1fr 4fr;
