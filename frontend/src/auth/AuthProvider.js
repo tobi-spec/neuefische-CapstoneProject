6fd36fbef25ec6a1a6c5cbd7e223a76095cd4d1b
Tobias Hahn <post@tobias-hahn.eu>
2021-09-08 10:44:45 +0200

    add AuthProvider

---
+++ frontend/src/auth/AuthProvider.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { useContext, useState } from 'react'
+import jwt from 'jsonwebtoken'
+import AuthContext from './AuthContext'
+
+export default function AuthProvider({ children }) {
+  const [token, setToken] = useState()
+
+  const login = credentials => {
+    axios
+      .post('/auth/accesstoken', credentials)
+      .then(reponse => reponse.data)
+      .then(dto => dto.token)
+  }
+
+  const logout = () => setToken()
+
+  const claims = jwt.decode(token)
+
+  const user = claims && {
+    username: claims.sub,
+    role: claims.role,
+  }
+
+  return (
+    <AuthContext.Provider value={{ token, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext)
