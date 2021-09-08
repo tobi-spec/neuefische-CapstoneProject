72ec082de0256c6aaa7e0b2dc78d81466ee59d68
Tobias Hahn <post@tobias-hahn.eu>
2021-09-08 12:28:17 +0200

    auth provider wrapper for router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Main from './pages/Main'
 import Login from './pages/Login'
 import ProtectedRoute from './auth/ProtectedRoute'
+import AuthProvider from './auth/AuthProvider'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Login} />
-        <ProtectedRoute path="/main" component={Main} />
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path="/" component={Login} />
+          <ProtectedRoute path="/main" component={Main} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   )
 }
 
