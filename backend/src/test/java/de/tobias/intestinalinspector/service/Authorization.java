940bc3b76f82e9c31a9f755c6f2fca9076a9866a
Tobias Hahn <post@tobias-hahn.eu>
2021-09-09 16:39:02 +0200

    add authorization methode for testing

---
+++ backend/src/test/java/de/tobias/intestinalinspector/service/Authorization.java
@@ -0,0 +1,45 @@
+package de.tobias.intestinalinspector.service;
+
+import de.tobias.intestinalinspector.config.JwtConfig;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class Authorization {
+
+    JwtConfig jwtConfig;
+
+    @Autowired
+    public Authorization(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
+    private HttpHeaders authorizedHeader(String username, String role){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+        Instant now = Instant.now();
+        Date iat = Date.from(now);
+        Date exp = Date.from(now.plus(Duration.ofMinutes(jwtConfig.getExpiresAfterMinutes())));
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(iat)
+                .setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS256, jwtConfig.getSecret())
+                .compact();
+
+        HttpHeaders header = new HttpHeaders();
+        header.setBearerAuth(token);
+
+        return header;
+    }
+}
