e9f1da712873e09b6dc087a377734870c6acf128
Tobias Hahn <post@tobias-hahn.eu>
2021-09-09 13:11:23 +0200

    add Content component outsourced css styles for page

---
+++ frontend/src/components/Content.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default styled.section`
+  display: grid;
+  height: 100vh;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+`
