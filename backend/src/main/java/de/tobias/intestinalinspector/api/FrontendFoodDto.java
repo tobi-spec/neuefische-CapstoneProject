c9d07a1aa644ff84accf8cec792812bd18b253eb
Tobias Hahn <post@tobias-hahn.eu>
2021-09-12 20:21:29 +0200

    add date to foodDto

--- backend/src/main/java/de/tobias/intestinalinspector/api/FrontendFoodDto.java
+++ backend/src/main/java/de/tobias/intestinalinspector/api/FrontendFoodDto.java
@@ -3,6 +3,8 @@ package de.tobias.intestinalinspector.api;
 
 import lombok.*;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -11,4 +13,5 @@ import lombok.*;
 public class FrontendFoodDto {
 
     String foodName;
+    Date date;
 }
