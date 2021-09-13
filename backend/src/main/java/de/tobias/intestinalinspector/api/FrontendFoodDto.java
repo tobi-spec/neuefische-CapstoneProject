09d6c17380720ed788ca40e5a6632e91ab1951c0
Tobias Hahn <post@tobias-hahn.eu>
2021-09-13 15:29:03 +0200

    integrationtest for add methode in foodcontroller fixed

--- backend/src/main/java/de/tobias/intestinalinspector/api/FrontendFoodDto.java
+++ backend/src/main/java/de/tobias/intestinalinspector/api/FrontendFoodDto.java
@@ -4,6 +4,7 @@ package de.tobias.intestinalinspector.api;
 import lombok.*;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -15,4 +16,26 @@ public class FrontendFoodDto {
     String foodName;
     long id;
     Date date;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FrontendFoodDto foodDto = (FrontendFoodDto) o;
+        return Objects.equals(foodName, foodDto.foodName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(foodName);
+    }
+
+    @Override
+    public String toString() {
+        return "FrontendFoodDto{" +
+                "foodName='" + foodName + '\'' +
+                ", id=" + id +
+                ", date=" + date +
+                '}';
+    }
 }
--- backend/src/main/java/de/tobias/intestinalinspector/api/FrontendFoodListDto.java
+++ backend/src/main/java/de/tobias/intestinalinspector/api/FrontendFoodListDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -19,4 +20,25 @@ public class FrontendFoodListDto {
     public void addFood(FrontendFoodDto frontendFoodDto){
         foodList.add(frontendFoodDto);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FrontendFoodListDto that = (FrontendFoodListDto) o;
+        return Objects.equals(foodList, that.foodList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(foodList);
+    }
+
+    @Override
+    public String toString() {
+        return "FrontendFoodListDto{" +
+                "foodList=" + foodList +
+                '}';
+    }
+
 }
