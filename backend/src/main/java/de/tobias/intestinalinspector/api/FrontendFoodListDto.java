3d6374af8453b9e65a08bea196a376736f5343a5
Tobias Hahn <post@tobias-hahn.eu>
2021-09-13 11:38:56 +0200

    add Dto for FoodList

---
+++ backend/src/main/java/de/tobias/intestinalinspector/api/FrontendFoodListDto.java
@@ -0,0 +1,24 @@
+package de.tobias.intestinalinspector.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FrontendFoodListDto {
+
+    List<FrontendFoodDto> foodList = new ArrayList<>();
+
+    public void addFood(FrontendFoodDto frontendFoodDto){
+        foodList.add(frontendFoodDto);
+    }
+
+
+}
