445a2c29dfad4efd1aa4715a281a9fb3f5e85a83
Tobias Hahn <post@tobias-hahn.eu>
2021-09-14 11:31:35 +0200

    create FindByUserName

--- backend/src/main/java/de/tobias/intestinalinspector/repository/PainRepository.java
+++ backend/src/main/java/de/tobias/intestinalinspector/repository/PainRepository.java
@@ -4,5 +4,9 @@ package de.tobias.intestinalinspector.repository;
 import de.tobias.intestinalinspector.model.PainEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PainRepository extends JpaRepository<PainEntity, Long> {
+
+    List<PainEntity> findAllByUserName(String username);
 }
