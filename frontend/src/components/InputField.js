e04db54d60e5eaf3de68598907093bb00c85a96e
Tobias Hahn <post@tobias-hahn.eu>
2021-09-14 10:42:05 +0200

    add attribute to InputField

--- frontend/src/components/InputField.js
+++ frontend/src/components/InputField.js
@@ -1,8 +1,21 @@
-export default function InputField({ title, type, name, value, onChange }) {
+export default function InputField({
+  title,
+  type,
+  name,
+  value,
+  onChange,
+  placeholder,
+}) {
   return (
     <div>
       <p>{title}</p>
-      <input type={type} name={name} value={value} onChange={onChange} />
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
     </div>
   )
 }
