dbae1621ce9b8c631ffdc7abe910d2458c33e387
Tobias Hahn <post@tobias-hahn.eu>
2021-09-14 11:29:00 +0200

    add equals/hascode

--- backend/src/main/java/de/tobias/intestinalinspector/api/FrontendPainDto.java
+++ backend/src/main/java/de/tobias/intestinalinspector/api/FrontendPainDto.java
@@ -2,6 +2,8 @@ package de.tobias.intestinalinspector.api;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,5 +15,16 @@ public class FrontendPainDto {
     private long id;
     private String date;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FrontendPainDto painDto = (FrontendPainDto) o;
+        return painLevel == painDto.painLevel;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(painLevel);
+    }
 }
