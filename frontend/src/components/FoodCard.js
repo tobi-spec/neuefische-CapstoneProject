5ac55cb8ddcb4492ac241421c4f298987d3d20f5
Tobias Hahn <post@tobias-hahn.eu>
2021-09-15 22:24:53 +0200

    create edit, delete and canel button, build in logic for edit and cancel

--- frontend/src/components/FoodCard.js
+++ frontend/src/components/FoodCard.js
@@ -1,17 +1,35 @@
 import styled from 'styled-components'
+import Button from './Button'
+import Edit from './Edit'
+import { useState } from 'react'
 
-export default function FoodCard({ foodName, date }) {
+export default function FoodCard({ foodName, date, id }) {
+  const [edit, setEdit] = useState(false)
+
+  const editHandler = () => {
+    setEdit(true)
+  }
+
+  const cancelHandler = () => {
+    setEdit(false)
+  }
+
   return (
     <Wrapper>
       <li>
         <p>food: {foodName}</p>
         <p>date: {date}</p>
+        {edit && <Edit id={id} />}
+        <Button onClick={editHandler}>Edit</Button>
+        {!edit && <Button>Delete</Button>}
+        {edit && <Button onClick={cancelHandler}>Cancel</Button>}
       </li>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.section`
+const Wrapper = styled.div`
+  display: flex;
   border-style: solid;
   border-radius: 10px;
   margin: 5px;
