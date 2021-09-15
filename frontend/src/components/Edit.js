e545928bf163ecf2e8ea6786762ad085f15d3fd8
Tobias Hahn <post@tobias-hahn.eu>
2021-09-15 22:25:30 +0200

    add Edit component

---
+++ frontend/src/components/Edit.js
@@ -0,0 +1,35 @@
+import InputField from './InputField'
+import Button from './Button'
+import styled from 'styled-components'
+import { useState } from 'react'
+import { useAuth } from '../auth/AuthProvider'
+import { getFood, updateFood } from '../service/AxiosService'
+
+export default function Edit({ id }) {
+  const { token } = useAuth()
+  const [newName, setNewName] = useState({ newName: '' })
+
+  const nameHandler = event =>
+    setNewName({
+      newName: event.target.value,
+    })
+
+  const nameSubmitHandler = event => {
+    event.preventDefault()
+    updateFood(id, newName, token)
+      .then(response => console.log(response))
+      .then(error => console.error(error))
+      .finally(() => setNewName(''))
+  }
+
+  return (
+    <Wrapper onSubmit={nameSubmitHandler}>
+      <InputField placeholder="new name" onChange={nameHandler} />
+      <Button>send</Button>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.form`
+  display: flex;
+`
