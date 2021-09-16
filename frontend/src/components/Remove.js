b11ac628746f8b3b7b96a59a42aeeeb8e156b778
Tobias Hahn <post@tobias-hahn.eu>
2021-09-16 11:34:11 +0200

    add logic in remove component

--- frontend/src/components/Remove.js
+++ frontend/src/components/Remove.js
@@ -1,10 +1,20 @@
 import Button from './Button'
+import { removeFood } from '../service/AxiosService'
+import { useAuth } from '../auth/AuthProvider'
 
-export default function Remove() {
+export default function Remove({ id }) {
+  const { token } = useAuth()
+
+  const yesHandler = () => {
+    removeFood(id, token)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
+
   return (
     <section>
       <p>Sure?</p>
-      <Button>Yes</Button>
+      <Button onClick={yesHandler}>Yes</Button>
       <Button>No</Button>
     </section>
   )
