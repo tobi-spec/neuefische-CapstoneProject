f1a3c428db764471382c7598ab880c7016bd89dc
Tobias Hahn <post@tobias-hahn.eu>
2021-09-16 11:33:52 +0200

    fix typo

--- frontend/src/service/AxiosService.js
+++ frontend/src/service/AxiosService.js
@@ -25,7 +25,7 @@ export const updateFood = (id, newName, token) =>
   axios.put(FoodPath + `/update=${id}`, newName, header(token))
 
 export const removeFood = (id, token) =>
-  axios.delete(FoodPath + `delete=${id}`, header(token))
+  axios.delete(FoodPath + `/delete=${id}`, header(token))
 
 export const addPain = (pain, token) =>
   axios.post(PainPath, pain, header(token))
