0fc51b9163c777e96e62a4671d456ed75cc74202
Tobias Hahn <post@tobias-hahn.eu>
2021-09-16 10:52:07 +0200

    basic delete component

---
+++ frontend/src/components/Delete.js
@@ -0,0 +1,11 @@
+import Button from './Button'
+
+export default function Delete() {
+  return (
+    <section>
+      <p>Sure?</p>
+      <Button>Yes</Button>
+      <Button>No</Button>
+    </section>
+  )
+}
