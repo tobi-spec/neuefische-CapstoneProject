f853684b15eeb1dc94335b486a20b57c3d674282
Tobias Hahn <post@tobias-hahn.eu>
2021-09-16 12:49:10 +0200

    switch from FoodCard to ItemCard

--- frontend/src/pages/FoodDiary.js
+++ frontend/src/pages/FoodDiary.js
@@ -5,7 +5,7 @@ import Footer from '../components/Footer'
 import { useEffect, useState } from 'react'
 import { getFood } from '../service/AxiosService'
 import { useAuth } from '../auth/AuthProvider'
-import FoodCard from '../components/FoodCard'
+import ItemCard from '../components/ItemCard'
 
 export default function FoodDiary() {
   const { token } = useAuth()
@@ -17,12 +17,13 @@ export default function FoodDiary() {
       .then(data => setFoodList(data.foodList))
   }, [token])
 
-  const foodItems = foodList.map(foodItem => (
-    <FoodCard
-      foodName={foodItem.foodName}
-      date={foodItem.date}
-      id={foodItem.id}
-      key={foodItem.id}
+  const Items = foodList.map(Item => (
+    <ItemCard
+      valueTitle={'Food'}
+      mainValue={Item.foodName}
+      date={Item.date}
+      id={Item.id}
+      key={Item.id}
     />
   ))
 
@@ -30,7 +31,7 @@ export default function FoodDiary() {
     <Wrapper>
       <Header title="Food Diary" />
       <Content>
-        <ul>{foodItems}</ul>
+        <ul>{Items}</ul>
       </Content>
       <Footer />
     </Wrapper>
