553ca21e14adbff12a486afd5d7b6007c06d3622
Tobias Hahn <post@tobias-hahn.eu>
2021-09-17 09:59:29 +0200

    add update and delete methode in painController

--- backend/src/main/java/de/tobias/intestinalinspector/controller/PainController.java
+++ backend/src/main/java/de/tobias/intestinalinspector/controller/PainController.java
@@ -1,12 +1,16 @@
 package de.tobias.intestinalinspector.controller;
 
 
+import de.tobias.intestinalinspector.api.FoodDto;
 import de.tobias.intestinalinspector.api.PainDto;
 import de.tobias.intestinalinspector.api.PainListDto;
+import de.tobias.intestinalinspector.api.UpdateDto;
 import de.tobias.intestinalinspector.model.AppUserEntity;
+import de.tobias.intestinalinspector.model.FoodEntity;
 import de.tobias.intestinalinspector.model.PainEntity;
 import de.tobias.intestinalinspector.service.PainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -33,15 +37,33 @@ public class PainController {
         PainEntity painToPersist = map(appUser, painDto);
         PainEntity persistedPain = painService.add(painToPersist);
         PainDto painToReturn = map(persistedPain);
-
         return  ok(painToReturn);
     }
 
     @GetMapping
     public ResponseEntity<PainListDto> getAll(@AuthenticationPrincipal AppUserEntity appUser){
-
         List<PainEntity> listOfPain = painService.getAll(appUser.getUserName());
         PainListDto painListDto = new PainListDto();
+        map(listOfPain, painListDto);
+        return ok(painListDto);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<PainDto> update(@PathVariable Long id, @RequestBody UpdateDto updateDto){
+        int newNumber = updateDto.getNewNumber();
+        PainEntity changedEntity = painService.update(id, newNumber);
+        PainDto returnDto = map(changedEntity);
+        return ok(returnDto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<PainDto> delete(@PathVariable Long id) {
+        PainEntity deleteEntity = painService.delete(id);
+        PainDto returnDto = map(deleteEntity);
+        return ok(returnDto);
+    }
+
+    private void map(List<PainEntity> listOfPain, PainListDto painListDto) {
         for(PainEntity painItem: listOfPain){
             PainDto painDto = PainDto.builder()
                     .painLevel(painItem.getPainLevel())
@@ -50,7 +72,6 @@ public class PainController {
                     .build();
             painListDto.addPain(painDto);
         }
-        return ok(painListDto);
     }
 
     private PainDto map(PainEntity persistedPain) {
