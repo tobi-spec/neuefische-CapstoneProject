68dc5588257279ee893cec56187bf6018453c7b8
Tobias Hahn <post@tobias-hahn.eu>
2021-09-17 13:53:14 +0200

    add cancelHandler to noHandler

--- frontend/src/components/Remove.js
+++ frontend/src/components/Remove.js
@@ -1,29 +1,28 @@
 import Button from './Button'
 import { removeFood } from '../service/AxiosService'
 import { useAuth } from '../auth/AuthProvider'
+import { Redirect } from 'react-router-dom'
+import { useEffect } from 'react'
 
-export default function Remove({ id, reloadList }) {
+export default function Remove({ id, reloadList, cancelHandler }) {
   const { token } = useAuth()
 
   const yesHandler = () => {
     removeFood(id, token)
       .then(response => console.log(response))
+      .then(() => reloadList(token))
       .catch(error => console.log(error))
   }
 
   const noHandler = () => {
-    reloadList()
+    cancelHandler()
   }
 
   return (
     <section>
       <p>Sure?</p>
-      <form onSubmit={yesHandler}>
-        <Button>Yes</Button>
-      </form>
-      <form onSubmit={noHandler}>
-        <Button>No</Button>
-      </form>
+      <Button onClick={yesHandler}> Yes </Button>
+      <Button onClick={noHandler}> No </Button>
     </section>
   )
 }
