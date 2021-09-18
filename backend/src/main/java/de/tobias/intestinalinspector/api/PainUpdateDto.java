329ec276460aafe510a7becc838164075bf55fc2
Tobias Hahn <post@tobias-hahn.eu>
2021-09-18 12:02:26 +0200

    make update dto for only pain use

---
+++ backend/src/main/java/de/tobias/intestinalinspector/api/PainUpdateDto.java
@@ -0,0 +1,13 @@
+package de.tobias.intestinalinspector.api;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PainUpdateDto {
+
+    private int newValue;
+}
