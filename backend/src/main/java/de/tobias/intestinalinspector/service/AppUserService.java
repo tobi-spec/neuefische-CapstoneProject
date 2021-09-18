5a9a0ccbaf766d456196d2b6418d5920354be238
Tobias Hahn <post@tobias-hahn.eu>
2021-09-18 14:35:36 +0200

    add set userrole and encode password

--- backend/src/main/java/de/tobias/intestinalinspector/service/AppUserService.java
+++ backend/src/main/java/de/tobias/intestinalinspector/service/AppUserService.java
@@ -3,19 +3,25 @@ package de.tobias.intestinalinspector.service;
 import de.tobias.intestinalinspector.model.AppUserEntity;
 import de.tobias.intestinalinspector.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppUserService {
 
-    AppUserRepository appUserRepository;
+    private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AppUserService(AppUserRepository appUserRepository) {
+    public AppUserService(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
         this.appUserRepository = appUserRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public AppUserEntity create(AppUserEntity newUser) {
+
+        newUser.setUserRole("user");
+        newUser.setUserPassword(passwordEncoder.encode(newUser.getUserPassword()));
         AppUserEntity createdEntity = appUserRepository.save(newUser);
         return createdEntity;
     }
