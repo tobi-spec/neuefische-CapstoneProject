f00f2713746f5dbde0f3fad01ff55ddce6e15485
Tobias Hahn <post@tobias-hahn.eu>
2021-09-19 18:18:14 +0200

    changed url controller paths, adjustet axios requests

--- frontend/src/service/AxiosService.js
+++ frontend/src/service/AxiosService.js
@@ -23,10 +23,10 @@ export const getFood = token =>
   axios.get(FoodPath, header(token)).then(response => response.data)
 
 export const updateFood = (id, newName, token) =>
-  axios.put(FoodPath + `/update/${id}`, newName, header(token))
+  axios.put(FoodPath + `/${id}`, newName, header(token))
 
 export const removeFood = (id, token) =>
-  axios.delete(FoodPath + `/delete/${id}`, header(token))
+  axios.delete(FoodPath + `/${id}`, header(token))
 
 export const addPain = (pain, token) =>
   axios.post(PainPath, pain, header(token))
@@ -35,7 +35,7 @@ export const getPain = token =>
   axios.get(PainPath, header(token)).then(response => response.data)
 
 export const updatePain = (id, newNumber, token) =>
-  axios.put(PainPath + `/update/${id}`, newNumber, header(token))
+  axios.put(PainPath + `/${id}`, newNumber, header(token))
 
 export const removePain = (id, token) =>
-  axios.delete(PainPath + `/delete/${id}`, header(token))
+  axios.delete(PainPath + `/${id}`, header(token))
