71002fde7a53852ad46a1f6fac78a6cbdd5d85d6
Tobias Hahn <post@tobias-hahn.eu>
2021-09-20 11:24:56 +0200

    add logic to registration page

--- frontend/src/pages/Registration.js
+++ frontend/src/pages/Registration.js
@@ -2,17 +2,43 @@ import styled from "styled-components";
 import Header from "../components/Header";
 import Content from "../components/Content";
 import InputField from "../components/InputField";
+import {useState} from "react";
+import Button from "../components/Button";
+import {createUser} from "../service/AxiosService";
 
 export default function Registration() {
-    return <Wrapper>
+    const [credentials, setCredentials] = useState({
+        userName: "",
+        userPassword: "",
+    })
+
+    const credentialsHandler  = event => {
+        setCredentials({...credentials, [event.target.name]: event.target.value});
+    }
+
+    const signSubmitHandler = event => {
+        event.preventDefault()
+        createUser(credentials).catch(error => console.error(error))
+    }
+
+    return <Wrapper onSubmit={signSubmitHandler}>
             <Header title="Registration"/>
             <Content>
-                <InputField title="Username"/>
-                <InputField title="Password"/>
+                <InputField
+                    title="Username"
+                    type="Text"
+                    value={credentials.userName}
+                    onChange={credentialsHandler }/>
+                <InputField
+                    title="Password"
+                    type="Text"
+                    value={credentials.userPassword}
+                    onChange={credentialsHandler }/>
+                <Button>sign-in</Button>
             </Content>
         </Wrapper>
 
 }
 
-const Wrapper = styled.div``
+const Wrapper = styled.form``
 
