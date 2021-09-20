c9a890c35eb8ba679effb9fc2bba143c29422c4a
Tobias Hahn <post@tobias-hahn.eu>
2021-09-20 10:35:59 +0200

    styling and links in welcome page

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,17 +1,45 @@
 import styled from "styled-components";
-import Content from "../components/Content";
-import Button from "../components/Button";
+import {Link} from "react-router-dom";
 
 export default function Welcome() {
     return<Wrapper>
-        <Content>
-            <h1>Intestinal Inspector</h1>
-            <h2>Welcome</h2>
-            <Button title=Log-in/>
-            <Button title=Sign-in/>
-        </Content>
+            <h1 className="main-title">Intestinal Inspector</h1>
+            <h1 className="sub-title">Welcome</h1>
+            <Link className="link" to="/login">Login</Link>
+            <Link className="link ">Register</Link>
     </Wrapper>
 }
 
 const Wrapper = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+  background-color: #138808;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  justify-items: center;
+  
+  
+  .main-title {
+    grid-row: 1;
+    font-size: 30px;
+  }
+  
+  
+  .link {
+    color: black;
+    text-decoration: none;
+    font-size: 20px;
+  }
+  
+  img {
+    grid-row: span 2;
+  }
+  
+  
+  
+  
+  
+  
 `
