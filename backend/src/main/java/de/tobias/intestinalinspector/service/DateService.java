5ae596cabf22354ea2fa29371e1b4a20f44fc97e
Tobias Hahn <post@tobias-hahn.eu>
2021-09-21 12:25:53 +0200

    removed not needed imports

--- backend/src/main/java/de/tobias/intestinalinspector/service/DateService.java
+++ backend/src/main/java/de/tobias/intestinalinspector/service/DateService.java
@@ -1,20 +1,12 @@
 package de.tobias.intestinalinspector.service;
 
-import de.tobias.intestinalinspector.api.FoodListDto;
 import de.tobias.intestinalinspector.model.FoodEntity;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalField;
-import java.time.temporal.WeekFields;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class DateService {
