702633789b1f11bcc8d53f051443ef1e074700d9
Tobias Hahn <post@tobias-hahn.eu>
2021-09-23 11:06:15 +0200

    make card render function to own const in ItemCardContainer

--- frontend/src/components/ItemCardContainer.js
+++ frontend/src/components/ItemCardContainer.js
@@ -2,31 +2,34 @@ import ItemCard from "./ItemCard";
 import styled from "styled-components";
 
 export default function ItemCardContainer({
-  key,
+  date,
   valueList,
   valueTitle,
   reloadList,
   editService,
   removeService}){
 
+    const cards = valueList.map(value => (
+        <ItemCard
+            mainValue= {value.foodName}
+            id = {value.id}
+            valueTitle={valueTitle}
+            reloadList={reloadList}
+            editService={editService}
+            removeService={removeService}
+        />
+    ))
+
     if( valueTitle === "Food"){
-        return valueList.map (Item =>
-            <Wrapper>
-                <h1> Day: {key}</h1>
-                <ItemCard
-                    mainValue= {Item.foodName}
-                    id = {Item.id}
-                    valueTitle={valueTitle}
-                    reloadList={reloadList}
-                    editService={editService}
-                    removeService={removeService}
-                />
+        return <Wrapper>
+                <h1> Day: {date}</h1>
+                {cards}
             </Wrapper>
-    )}
+    }
     if( valueTitle === "Pain"){
         return valueList.map (Item =>
             <Wrapper>
-                <h1> Day: {key}</h1>
+                <h1> Day: {date}</h1>
                 <ItemCard
                     mainValue = {Item.painLevel}
                     id = {Item.id}
