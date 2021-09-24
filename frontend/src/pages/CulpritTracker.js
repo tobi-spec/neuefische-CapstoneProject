cf9266571fafc965526f719b04e9de8d82b3f0ce
Tobias Hahn <post@tobias-hahn.eu>
2021-09-24 11:28:22 +0200

    init CulpritTracker page

---
+++ frontend/src/pages/CulpritTracker.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function CulpritTracker(){
+    return <Wrapper>
+        <Header title="Culprit Tracker"/>
+        <Footer/>
+    </Wrapper>
+}
+
+const Wrapper = styled.div``
