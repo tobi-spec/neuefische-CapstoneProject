d2659f694a173e24a656b169751977d87891ed2c
Tobias Hahn <post@tobias-hahn.eu>
2021-09-25 19:48:37 +0200

    build in clickEvent + refactoring

--- frontend/src/components/LineChart.js
+++ frontend/src/components/LineChart.js
@@ -1,51 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import {Line} from 'react-chartjs-2'
-import styled from "styled-components/macro";
 
 export default function LineChart({yValues, xValues}){
+    const [clickedElement, setClickedElement] = useState("")
 
     const xAxes = xValues()
     const yAxes = yValues()
 
-const LineChart = () => {
-    return (
-        <Wrapper>
-            <Line
-                data={{
-                    labels: xAxes,
-                    datasets: [{
-                        label: "trace of the perpetrators",
-                        data: yAxes,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1,
-                    }]
-                }}
-                height={200}
-                width={300}
-            />
-        </Wrapper>
+    const  data={
+                labels: xAxes,
+                    datasets: [
+                        {
+                            label: "trace of the perpetrators",
+                            data: yAxes,
+                            backgroundColor: ['rgba(11, 156, 49, 0.2)'],
+                            borderColor: ['rgba(11, 156, 49, 0.2)'],
+                            borderWidth: 5,
+                        }
+                    ],
+                }
+
+    const options={}
+
+    const getElementAtEvent = element => {
+        if (!element.length) return;
+
+        const { datasetIndex, index } = element[0];
+        setClickedElement(
+            `${data.labels[index]} - ${data.datasets[datasetIndex].data[index]}`
+        );
+    };
+
+    const LineChart = () => {
+        return (<div>
+            <Line data={data} options={options} height={100} width={150} getElementAtEvent={getElementAtEvent}/>
+            <p>{clickedElement}</p>
+            </div>
     )
+    }
+
+    return LineChart()
 }
 
-return LineChart()
-}
-
-const Wrapper = styled.div`
-
-`
 
