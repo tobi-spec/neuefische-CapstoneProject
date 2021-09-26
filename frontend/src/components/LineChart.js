5612ada75773b338bcc8a47ff4d27f2b0545f701
Tobias Hahn <post@tobias-hahn.eu>
2021-09-26 16:36:58 +0200

    add clickevent in LineChart, showing eaten food of date

--- frontend/src/components/LineChart.js
+++ frontend/src/components/LineChart.js
@@ -1,8 +1,34 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Line} from 'react-chartjs-2'
+import {useAuth} from "../auth/AuthProvider";
+import {getFood, removeFood, updateFood} from "../service/AxiosService";
+import ItemGallery from "./ItemGallery";
 
 export default function LineChart({yValues, xValues}){
-    const [clickedElement, setClickedElement] = useState("")
+    const {token} = useAuth()
+    const [date, setDate] = useState("")
+    const [foodMaps, setFoodMaps] = useState([])
+
+    useEffect(() => {
+        getFood(token)
+            .then(data => setFoodMaps(data.foodMaps))
+    }, [token])
+
+    const reloadFoodList = token => {
+        getFood(token)
+            .then(data => setFoodMaps(data.foodMaps))
+    }
+
+    const getElementAtEvent = element => {
+        if (!element.length) return;
+
+        const { index } = element[0];
+        setDate(
+            `${data.labels[index]}`
+        );
+    };
+
+    const filterData = foodMaps.filter(map => map.date === date)
 
     const xAxes = xValues()
     const yAxes = yValues()
@@ -20,23 +46,20 @@ export default function LineChart({yValues, xValues}){
                     ],
                 }
 
-    const options={}
-
-    const getElementAtEvent = element => {
-        if (!element.length) return;
-
-        const { datasetIndex, index } = element[0];
-        setClickedElement(
-            `${data.labels[index]} - ${data.datasets[datasetIndex].data[index]}`
-        );
-    };
-
     const LineChart = () => {
-        return (<div>
-            <Line data={data} options={options} height={100} width={150} getElementAtEvent={getElementAtEvent}/>
-            <p>{clickedElement}</p>
+        return (
+            <div>
+            <div>
+            <Line data={data} height={100} width={150} getElementAtEvent={getElementAtEvent}/>
             </div>
-    )
+                <ItemGallery
+                    itemMaps={filterData}
+                    valueTitle={"Food"}
+                    reloadList={reloadFoodList}
+                    editService={updateFood}
+                    removeService={removeFood}
+                />
+            </div>)
     }
 
     return LineChart()
