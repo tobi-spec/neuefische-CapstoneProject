cd59528e4a7bbdda2f4e9dabbc1572711ffc6c7d
Tobias Hahn <post@tobias-hahn.eu>
2021-09-28 19:51:45 +0200

    create deleteUser methode in appUserService

--- backend/src/main/java/de/tobias/intestinalinspector/service/AppUserService.java
+++ backend/src/main/java/de/tobias/intestinalinspector/service/AppUserService.java
@@ -2,10 +2,13 @@ package de.tobias.intestinalinspector.service;
 
 import de.tobias.intestinalinspector.model.AppUserEntity;
 import de.tobias.intestinalinspector.repository.AppUserRepository;
+import de.tobias.intestinalinspector.repository.FoodRepository;
+import de.tobias.intestinalinspector.repository.PainRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.Optional;
@@ -14,11 +17,15 @@ import java.util.Optional;
 public class AppUserService {
 
     private final AppUserRepository appUserRepository;
+    private final FoodRepository foodRepository;
+    private final PainRepository painRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AppUserService(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
+    public AppUserService(AppUserRepository appUserRepository, FoodRepository foodRepository, PainRepository painRepository, PasswordEncoder passwordEncoder) {
         this.appUserRepository = appUserRepository;
+        this.foodRepository = foodRepository;
+        this.painRepository = painRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -43,6 +50,19 @@ public class AppUserService {
             return appUserRepository.save(entityToChange.get());
         }else {
             throw new EntityNotFoundException("Not such entry found");
+        }
+    }
+
+    @Transactional
+    public AppUserEntity deleteUser(String userName) {
+        Optional<AppUserEntity> userToDelete = appUserRepository.findByUserName(userName);
+        if (userToDelete.isPresent()) {
+            appUserRepository.deleteAppUserEntityByUserName(userName);
+            foodRepository.deleteAllByUserName(userName);
+            painRepository.deleteAllByUserName(userName);
+            return userToDelete.get();
+        } else {
+            throw new EntityNotFoundException("No such entry found");
         }
     }
 }
