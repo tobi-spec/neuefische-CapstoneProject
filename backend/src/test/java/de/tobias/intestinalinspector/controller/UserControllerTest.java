cad3edb67047b0eb9d883086b0028d434003d859
Tobias Hahn <post@tobias-hahn.eu>
2021-09-28 20:25:51 +0200

    create test for deleteAccount

--- backend/src/test/java/de/tobias/intestinalinspector/controller/UserControllerTest.java
+++ backend/src/test/java/de/tobias/intestinalinspector/controller/UserControllerTest.java
@@ -5,8 +5,6 @@ import de.tobias.intestinalinspector.api.AccessTokenDto;
 import de.tobias.intestinalinspector.api.CredentialsDto;
 import de.tobias.intestinalinspector.api.NewPassword;
 import de.tobias.intestinalinspector.api.UserDto;
-import de.tobias.intestinalinspector.model.AppUserEntity;
-import de.tobias.intestinalinspector.repository.AppUserRepository;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -90,8 +88,8 @@ class UserControllerTest {
     public void testChangePassword() {
         //GIVEN
         UserDto userToCreate = UserDto.builder()
-                .userName("Julia")
-                .userPassword("Julia123")
+                .userName("Maria")
+                .userPassword("Maria123")
                 .build();
         NewPassword newPassword = new NewPassword("54321");
 
@@ -129,6 +127,34 @@ class UserControllerTest {
         assertEquals(HttpStatus.OK, actualResponseLogin.getStatusCode());
         assertThat(actualResponseLogin.getBody().getToken(), is(not("")));
 
+    }
+
+    @Test
+    @Order(4)
+    public void testDeleteAccount(){
+        //GIVEN
+        UserDto userToCreate = UserDto.builder()
+                .userName("Julia")
+                .userPassword("Julia123")
+                .build();
+        //WHEN
+        //create User
+        HttpEntity<UserDto> httpEntityCreate = new HttpEntity<>(userToCreate);
+        ResponseEntity<UserDto> actualResponseCreateUser = testRestTemplate.exchange(url()+"/api/user",
+                                                                                    HttpMethod.POST,
+                                                                                    httpEntityCreate,
+                                                                                    UserDto.class);
+        //delete User
+        HttpEntity<UserDto> httpEntityDelete = new HttpEntity<>(testAuthorization.Header("Julia", "user"));
+        ResponseEntity<UserDto> actualResponseDeleteAccount = testRestTemplate.exchange( url() + "/api/user/deleteAccount",
+                                                                                        HttpMethod.DELETE,
+                                                                                        httpEntityDelete,
+                                                                                        UserDto.class);
+        //THEN
+        assertEquals(HttpStatus.OK, actualResponseCreateUser.getStatusCode());
+        assertEquals(HttpStatus.OK, actualResponseDeleteAccount.getStatusCode());
+        assertThat(actualResponseDeleteAccount.getBody(), is(not("")));
+        assertEquals(userToCreate.getUserName(), actualResponseDeleteAccount.getBody().getUserName());
     }
 
 
