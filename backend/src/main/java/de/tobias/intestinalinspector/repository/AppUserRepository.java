fe55017e7a9b5a5f828f79c7c7592714358b1c61
Tobias Hahn <post@tobias-hahn.eu>
2021-09-28 11:41:21 +0200

    create changePassword methode in UserRepo

--- backend/src/main/java/de/tobias/intestinalinspector/repository/AppUserRepository.java
+++ backend/src/main/java/de/tobias/intestinalinspector/repository/AppUserRepository.java
@@ -2,10 +2,20 @@ package de.tobias.intestinalinspector.repository;
 
 import de.tobias.intestinalinspector.model.AppUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUserEntity, Long> {
 
     Optional<AppUserEntity> findByUserName(String name);
 
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE AppUserEntity u SET u.userPassword = :setNewPassword WHERE u.userName = :userName")
+    void updatePasswordOfUser(@Param("userName") String userName, @Param("setNewPassword") String setNewPassword);
 }
