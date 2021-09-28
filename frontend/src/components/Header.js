78716a5f4e0b2360dea8eafcfecfb9a65fc2f0b3
Tobias Hahn <post@tobias-hahn.eu>
2021-09-28 10:08:50 +0200

    conditional for back and logout button in header, based on location

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,13 +2,17 @@ import styled from 'styled-components'
 import BackButton from "./BackButton";
 import ProfileButton from "./ProfileButton";
 import {useAuth} from "../auth/AuthProvider";
+import {useLocation} from "react-router-dom";
+import LogoutButton from "./LogoutButton";
 
 export default function Header({ title }) {
     const {user} = useAuth();
 
+    const location = useLocation()
+
   return (
     <Wrapper>
-        <BackButton/>
+        { location.pathname === "/main" ? <LogoutButton/> : <BackButton/>}
       <h1>{title}</h1>
         { user && <ProfileButton/>}
     </Wrapper>
